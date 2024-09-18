7a963d6737b2fdf115f3328218cfce499120e607
Karem Gobran <112480829+Karemgobran@users.noreply.github.com>
2024-09-18 18:22:13 +0300

    add responsive nav

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ const responsive_nav = document.querySelector(".responsive-nav");
 const navb_btn = document.querySelector(".navb");
 
 navb_btn.addEventListener("click", () => {
-  responsive_nav.style.cssText = `right:0 ; z-index:1`;
+  responsive_nav.style.cssText = `right:0 ; `;
 });
-const exit = document.querySelector(".ex");
+
+const exit = document.querySelector(".nav-exit");
 
 exit.addEventListener("click", () => {
-  responsive_nav.style.cssText = `right:-250px z-index:-1;`;
+  responsive_nav.style.cssText = `right:-250px ; `;
 });
