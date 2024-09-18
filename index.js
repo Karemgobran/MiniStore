493cc3d081bb952a5c57ad59301123870d31bada
Karem Gobran <112480829+Karemgobran@users.noreply.github.com>
2024-09-19 02:31:26 +0300

    add scroll js code

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ const exit = document.querySelector(".nav-exit");
 exit.addEventListener("click", () => {
   responsive_nav.style.cssText = `right:-250px ; `;
 });
+
+// ---------- section scroll -------------------------------------
+const productContainers = [...document.querySelectorAll(".page")];
+const nxtBtn = [...document.querySelectorAll("#nxt-btn")];
+const preBtn = [...document.querySelectorAll("#pre-btn")];
+
+productContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
+
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
